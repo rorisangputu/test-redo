aa37e053199653fc22845c26ce657eaecde37f7c
Rorisang Putu <rorisangputu@gmail.com>
2024-12-02 21:08:36 +0200

    adding animation

--- src/Utils/framer.js
+++ src/Utils/framer.js
@@ -11,6 +11,31 @@ export const SlideUp = (delay) => {
                 duration: 0.3,
                 delay: delay,
                 ease: "easeInOut",
+                type: "spring", // Add a spring-based bounce
+            damping: 10, // Reduces oscillations
+            bounce: 0.3, // Adjust the bounce effect (0-1)
+            
+            }
+        },
+    }
+}
+export const SlideDown = (delay) => {
+    return {
+        initial: {
+            opacity: 0,
+            y: -50
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.3,
+                delay: delay,
+                ease: "easeInOut",
+                type: "spring", // Add a spring-based bounce
+            damping: 10, // Reduces oscillations
+            bounce: 0.3, // Adjust the bounce effect (0-1)
+            
             },
         },
     }
